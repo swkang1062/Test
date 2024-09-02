a6cc2679f2178f6295256ba9cf1d5f260dd98677
swkang1062 <34428669+swkang1062@users.noreply.github.com>
2024-09-02 13:13:33 +0900

    Update mk_openai.py for latest LangChain with OpenAI
    LangChain 의 load_qa_chain() 을 사용할 경우,
    OpenAI 호출 client.chat.completions.create wrapping이 되지 않음.
    
    이에 대한 수정으로 다음을 wrapping 함
    @wrapt.patch_function_wrapper("langchain_community.chat_models","ChatOpenAI.completion_with_retry")

--- mk_openai.py
+++ mk_openai.py
@@ -12,3 +12,23 @@ def wrap_chat_completion(wrapped, instance, args, kwargs):
     print("===================================")
 
     return response
+
+
+# LangChain 최신 버젼 (2024/Sep/02)
+@wrapt.patch_function_wrapper("langchain_community.chat_models","ChatOpenAI.completion_with_retry")
+def log_tokens(wrapped, instance, args, kwargs):
+    # 원본 함수 호출
+    response = wrapped(*args, **kwargs)
+    print("=====================", flush=True)
+    print("여기서 사용한 Token 수룰 남기면, App. 에서 실수로 남기지 않는 일이 없어진다. ~~~ ")
+    # print("response['usage'] ==== ", response['usage'])
+    print("response == ", response.usage)
+    usage=response.usage
+    print("completion_tokens ==== ", usage.completion_tokens)
+    print("prompt_tokens == ", usage.prompt_tokens)
+    print("total_tokens == ", usage.total_tokens)
+
+    print("===================================")
+    
+
+    return response
